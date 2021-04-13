a405bf024c55fa8e13ddc70e9b29c7d9f7bff315
Maliha Tanweer <malihatanweer@gmail.com>
2021-04-13 11:29:19 -0400

    Code cleanup

--- js/index.js
+++ js/index.js
@@ -14,57 +14,9 @@ function initMap() {
   }
 }
 
-// $(document).ready(function(){
-// 	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
-// 		$(this).toggleClass('open');
-// 	});
-// });
-
-// console.log(document.documentElement.clientWidth);
-// console.log(window.screen.availWidth);
-// console.log(window.innerWidth);
-// window.addEventListener('resize', function() {
-//   if (window.innerWidth > 580) {
-//     document.querySelector('.hidden-navbar').style.display = 'none';
-//   }
-// });
-// ----------------
-// document.getElementById('nav-icon3').addEventListener('click', function() {
-//   this.classList.toggle('open');  
-//   document.querySelector('.hidden-navbar').style.display = 'none';
-//   if (this.classList.contains('open')) {
-//       document.querySelector('.hidden-navbar').style.display = 'block';
-//   } 
-// });
-// -------------------------------  https://codepen.io/designcouch/pen/Atyop
 document.getElementById('nav-icon3').addEventListener('click', function() {
   this.classList.toggle('open');  
   document.querySelector('.hidden-navbar').classList.toggle('open');
 });
 
 
-// document.getElementById('nav-icon3').addEventListener('click', function() {
-//   document.querySelector('.hidden-navbar').style.display = 'block';
-// });
-// ***************************
-// document.querySelector('i.fa.fa-bars').addEventListener('click', function() {
-//   document.querySelector('.hidden-navbar').style.display = 'block';
-// });
-// document.querySelector('i.fa.fa-times').addEventListener('click', function() {
-//   document.querySelector('.hidden-navbar').style.display = 'none';
-// });
-
-// function initMap() {
-//     // The location of Uluru
-//     const position = { lat: 40.766670, lng: -73.913100 };
-//     // The map, centered at Uluru
-//     const secondMap = new google.maps.Map(document.getElementById("map-2"), {
-//       zoom: 8,
-//       center: position,
-//     });
-//     // The marker, positioned at Uluru
-//     const marker = new google.maps.Marker({
-//       position: position,
-//       map: secondMap,
-//     });
-// }
